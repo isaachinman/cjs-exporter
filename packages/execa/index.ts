693a9a0b2b437e445991ce1f6dee79d511913a09
kastov <kastov@gog.sh>
2024-12-23 15:54:10 +0300

    refactor: enhance execa exports and add Prettier configuration

--- packages/execa/index.ts
+++ packages/execa/index.ts
@@ -1,1 +1,26 @@
-export * from "execa";
+import {
+  execa as execaOriginal,
+  execaSync as execaSyncOriginal,
+  execaCommand as execaCommandOriginal,
+  execaCommandSync as execaCommandSyncOriginal,
+  execaNode as execaNodeOriginal,
+  $ as $Original,
+  ExecaError,
+  ExecaSyncError,
+  parseCommandString,
+  sendMessage,
+  getOneMessage,
+  getEachMessage,
+  getCancelSignal,
+} from 'execa'
+
+export { parseCommandString }
+export { ExecaError, ExecaSyncError }
+
+export const execa = execaOriginal
+export const execaSync = execaSyncOriginal
+export const execaCommand = execaCommandOriginal
+export const execaCommandSync = execaCommandSyncOriginal
+export const execaNode = execaNodeOriginal
+export const $ = $Original
+export { sendMessage, getOneMessage, getEachMessage, getCancelSignal }
